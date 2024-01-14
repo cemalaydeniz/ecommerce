4383582775191cbedefc09941682435b90434c4a
edunimbus <106919349+cemalaydeniz@users.noreply.github.com>
2024-01-14 15:45:46 +0100

    created base of ticket message class

---
+++ src/Core/ecommerce.Domain/Aggregates/OrderRepository/Entities/TicketMessage.cs
@@ -0,0 +1,15 @@
+﻿using ecommerce.Domain.SeedWork;
+
+namespace ecommerce.Domain.Aggregates.OrderRepository.Entities
+{
+    public class TicketMessage : BaseEntity<Guid>
+    {
+        #region Properties
+        public string Content { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+
+        // Navigations
+        public Guid UserId { get; private set; }
+        #endregion
+    }
+}
