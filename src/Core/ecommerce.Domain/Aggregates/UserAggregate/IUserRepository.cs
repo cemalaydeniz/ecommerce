85a7e594f0c5c495953c18f1d19cadf3ae302e73
edunimbus <106919349+cemalaydeniz@users.noreply.github.com>
2024-01-14 15:26:04 +0100

    created user repository

---
+++ src/Core/ecommerce.Domain/Aggregates/UserAggregate/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace ecommerce.Domain.Aggregates.UserAggregate
+{
+    /// <summary>
+    /// Available CRUD operations for the aggregate root: <see cref="User"/>
+    /// </summary>
+    public interface IUserRepository
+    {
+        Task AddAsync(User newUser, CancellationToken cancellationToken = default);
+        void Update(User user);
+        Task<User?> GetByIdAsync(Guid userId, bool includeRoles, bool getSoftDeleted, CancellationToken cancellationToken = default);
+        Task<User?> GetByEmailAsync(string email, bool includeRoles, bool getSoftDeleted, CancellationToken cancellationToken = default);
+    }
+}
