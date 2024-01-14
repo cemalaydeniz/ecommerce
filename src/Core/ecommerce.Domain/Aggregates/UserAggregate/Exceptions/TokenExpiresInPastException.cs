3f8c8306dc39522ee18796919b831ddb33b9c3d1
edunimbus <106919349+cemalaydeniz@users.noreply.github.com>
2024-01-14 19:15:20 +0100

    created token value object for the user class
    This value object will hold the refresh token

---
+++ src/Core/ecommerce.Domain/Aggregates/UserAggregate/Exceptions/TokenExpiresInPastException.cs
@@ -0,0 +1,8 @@
+﻿namespace ecommerce.Domain.Aggregates.UserAggregate.Exceptions
+{
+    public class TokenExpiresInPastException : Exception
+    {
+        public TokenExpiresInPastException() : base() { }
+        public TokenExpiresInPastException(string message) : base(message) { }
+    }
+}
---
+++ src/Core/ecommerce.Domain/Aggregates/UserAggregate/ValueObjects/Token.cs
@@ -0,0 +1,43 @@
+﻿using ecommerce.Domain.Aggregates.UserAggregate.Exceptions;
+using ecommerce.Domain.SeedWork;
+
+namespace ecommerce.Domain.Aggregates.UserAggregate.ValueObjects
+{
+    public class Token : ValueObject
+    {
+        public string? Value { get; private set; }
+        public DateTime? ExpiresAt { get; private set; }
+
+        private Token() { }
+
+        /// <summary>
+        /// Creates a new token locally
+        /// </summary>
+        /// <param name="value">The value of the token</param>
+        /// <param name="expiresAt">The expiration date of the token</param>
+        /// <exception cref="TokenExpiresInPastException"></exception>
+        public Token(string? value, DateTime? expiresAt)
+        {
+            ValidateExpirationDate(expiresAt);
+
+            Value = value;
+            ExpiresAt = expiresAt;
+        }
+
+        public override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Value ?? string.Empty;
+        }
+
+        #region Validations
+        private void ValidateExpirationDate(DateTime? expiresAt)
+        {
+            if (expiresAt == null)
+                return;
+
+            if (expiresAt <= DateTime.UtcNow)
+                throw new TokenExpiresInPastException("The date in the past according to UTC-0");
+        }
+        #endregion
+    }
+}
