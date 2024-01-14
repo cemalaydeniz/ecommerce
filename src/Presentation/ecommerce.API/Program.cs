5f078f7a7380b0f24980056088fed1437245bd31
edunimbus <106919349+cemalaydeniz@users.noreply.github.com>
2024-01-14 23:20:02 +0100

    added jwt ui to the swagger

--- src/Presentation/ecommerce.API/Program.cs
+++ src/Presentation/ecommerce.API/Program.cs
@@ -7,6 +7,7 @@ using ecommerce.Persistence.Authentication;
 using ecommerce.Persistence.Seeding;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,7 +18,37 @@ builder.Services.AddControllers(_ =>
     _.Filters.Add(typeof(ReformatValidationProblemFilter));
 });
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(_ =>
+{
+    _.SwaggerDoc("v1", new OpenApiInfo()
+    {
+        Title = "ecommerce",
+        Version = "v1"
+    });
+
+    _.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Description = "Jwt Bearer Token",
+        Type = SecuritySchemeType.ApiKey,
+        In = ParameterLocation.Header
+    });
+
+    _.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme()
+            {
+                Reference = new OpenApiReference()
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] { }
+        }
+    });
+});
 
 builder.Services.AddApplicationServices();
 builder.Services.AddPersistenceServices(builder.Configuration);
