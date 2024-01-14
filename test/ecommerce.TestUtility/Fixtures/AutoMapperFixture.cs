2e07f8917e06f60b2e9a78e82204278c026ef226
edunimbus <106919349+cemalaydeniz@users.noreply.github.com>
2024-01-15 00:29:34 +0100

    added automapper fixture for tests

---
+++ test/ecommerce.TestUtility/Fixtures/AutoMapperFixture.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using System.Reflection;
+
+namespace ecommerce.TestUtility.Fixtures
+{
+    public class AutoMapperFixture : IDisposable
+    {
+        public IMapper Mapper { get; private set; }
+        public AutoMapperFixture()
+        {
+            MapperConfiguration config = new MapperConfiguration(_ =>
+            {
+                _.AddMaps(typeof(Application.ServiceRegistration).GetTypeInfo().Assembly);
+            });
+
+            Mapper = config.CreateMapper();
+        }
+
+        public void Dispose() { }
+    }
+}
