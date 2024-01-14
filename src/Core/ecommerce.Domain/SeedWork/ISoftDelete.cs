3efb1b25d964b506137f31f7670ac43ea7e605e4
edunimbus <106919349+cemalaydeniz@users.noreply.github.com>
2024-01-14 14:45:00 +0100

    created soft delete interface
    In order to give soft delete ability to the entities

---
+++ src/Core/ecommerce.Domain/SeedWork/ISoftDelete.cs
@@ -0,0 +1,12 @@
+﻿namespace ecommerce.Domain.SeedWork
+{
+    /// <summary>
+    /// Declares an entity as soft deletable, giving the impleted class the property <see cref="IsDeleted"/>
+    /// </summary>
+    public interface ISoftDelete
+    {
+        bool Delete();
+
+        bool IsDeleted { get; }
+    }
+}
