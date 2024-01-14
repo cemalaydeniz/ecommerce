127830b45d6df72614bead2c86a85136f9897ff3
edunimbus <106919349+cemalaydeniz@users.noreply.github.com>
2024-01-14 14:40:49 +0100

    created base entity class

---
+++ src/Core/ecommerce.Domain/SeedWork/BaseEntity.cs
@@ -0,0 +1,39 @@
+﻿#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+namespace ecommerce.Domain.SeedWork
+{
+    /// <summary>
+    /// Base class of the entites that are not aggregate roots
+    /// </summary>
+    /// <typeparam name="TKey">The type of the Id of the entity</typeparam>
+    public class BaseEntity<TKey> : IEquatable<BaseEntity<TKey>>
+        where TKey : notnull
+    {
+        public TKey Id { get; protected set; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is BaseEntity<TKey> other && this.Id.Equals(other.Id);
+        }
+
+        public bool Equals(BaseEntity<TKey>? other)
+        {
+            return Equals((object?)other);
+        }
+
+        public static bool operator ==(BaseEntity<TKey>? left, BaseEntity<TKey>? right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(BaseEntity<TKey>? left, BaseEntity<TKey>? right)
+        {
+            return !Equals(left, right);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+    }
+}
