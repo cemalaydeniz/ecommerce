3053fbe7640f83128d71b1524b7c01a5b95aa3af
edunimbus <106919349+cemalaydeniz@users.noreply.github.com>
2024-01-14 18:39:04 +0100

    configured the API project for the migration

--- src/Presentation/ecommerce.API/Program.cs
+++ src/Presentation/ecommerce.API/Program.cs
@@ -1,15 +1,17 @@
+using ecommerce.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+//~ Begin - Services
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddPersistenceServices(builder.Configuration);
+//~ End
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
