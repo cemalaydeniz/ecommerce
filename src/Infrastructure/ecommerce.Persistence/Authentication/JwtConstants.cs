38cb81dfb77ab02d873023c85c19da177b791960
edunimbus <106919349+cemalaydeniz@users.noreply.github.com>
2024-01-14 19:42:30 +0100

    implemented the jwt token service
    It is used to generate both access token and refresh token

---
+++ src/Infrastructure/ecommerce.Persistence/Authentication/JwtConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace ecommerce.Persistence.Authentication
+{
+    public static class JwtConstants
+    {
+        public const string Key_ConfigKey = "JwtSettings:Key";
+        public const string Issuer_ConfigKey = "JwtSettings:Issuer";
+        public const string Audience_ConfigKey = "JwtSettings:Audience";
+        public const string AcessTokenLifespanInMinutes_ConfigKey = "JwtSettings:AccessTokenLifespanInMinutes";
+        public const string RefreshTokenLifespanInDays_ConfigKey = "JwtSettings:RefreshTokenLifespanInDays";
+    }
+}
---
+++ src/Infrastructure/ecommerce.Persistence/Authentication/JwtTokenService.cs
@@ -0,0 +1,72 @@
+﻿using ecommerce.Application.Authentication;
+using ecommerce.Application.Services;
+using ecommerce.Domain.Aggregates.UserAggregate;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ecommerce.Persistence.Authentication
+{
+    public class JwtTokenService : IJwtTokenService
+    {
+        private readonly IConfiguration _configuration;
+
+        public JwtTokenService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public JwtToken GenerateToken(User user, bool newRefreshToken = false)
+        {
+            var jwtSecretKey = _configuration[JwtConstants.Key_ConfigKey];
+            var signInKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSecretKey!));
+
+            List<Claim> claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Email, user.Email ?? string.Empty)
+            };
+            foreach (var role in user.Roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role.Name));
+            }
+
+            var issuer = _configuration[JwtConstants.Issuer_ConfigKey];
+            var audience = _configuration[JwtConstants.Audience_ConfigKey];
+            var accessTokenLifeSpan = _configuration[JwtConstants.AcessTokenLifespanInMinutes_ConfigKey];
+            JwtSecurityToken securityToken = new JwtSecurityToken(
+                issuer: issuer,
+                audience: audience,
+                expires: DateTime.UtcNow.AddMinutes(int.Parse(accessTokenLifeSpan!)),
+                signingCredentials: new SigningCredentials(signInKey, SecurityAlgorithms.HmacSha256),
+                claims: claims);
+
+            JwtToken token = new JwtToken()
+            {
+                AccessToken = new JwtSecurityTokenHandler().WriteToken(securityToken)
+            };
+
+            if (newRefreshToken)
+            {
+                var refreshTokenLifeSpan = _configuration[JwtConstants.RefreshTokenLifespanInDays_ConfigKey];
+                token.RefreshToken = GenerateRefreshToken();
+                token.RefreshTokenExpirationDate = DateTime.UtcNow.AddDays(int.Parse(refreshTokenLifeSpan!));
+            }
+
+            return token;
+        }
+
+        private string GenerateRefreshToken()
+        {
+            byte[] number = new byte[32];
+            using (RandomNumberGenerator random = RandomNumberGenerator.Create())
+            {
+                random.GetBytes(number);
+                return Convert.ToBase64String(number);
+            }
+        }
+    }
+}
