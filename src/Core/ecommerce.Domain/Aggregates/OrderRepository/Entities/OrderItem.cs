6b98c072af5ae949bfd5139c3e5953b7f42b4b98
edunimbus <106919349+cemalaydeniz@users.noreply.github.com>
2024-01-14 15:44:15 +0100

    created base of order item class

---
+++ src/Core/ecommerce.Domain/Aggregates/OrderRepository/Entities/OrderItem.cs
@@ -0,0 +1,17 @@
+﻿using ecommerce.Domain.Common.ValueObjects;
+using ecommerce.Domain.SeedWork;
+
+namespace ecommerce.Domain.Aggregates.OrderRepository.Entities
+{
+    public class OrderItem : BaseEntity<Guid>
+    {
+        #region Properties
+        public string ProductName { get; private set; }
+        public Money UnitPrice { get; private set; }
+        public int Quantity { get; private set; }
+
+        // Navigations
+        public Guid ProductId { get; private set; }
+        #endregion
+    }
+}
