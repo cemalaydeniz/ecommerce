428382436da9115453f99444ca3c6f40e551537e
edunimbus <106919349+cemalaydeniz@users.noreply.github.com>
2024-01-14 15:39:53 +0100

    created ticket status enum for order class

---
+++ src/Core/ecommerce.Domain/Aggregates/OrderRepository/Enums/TicketStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace ecommerce.Domain.Aggregates.OrderRepository.Enums
+{
+    public enum TicketStatus : byte
+    {
+        NotOpened       =   0,
+        Opened          =   1,
+        Closed          =   2
+    }
+}
