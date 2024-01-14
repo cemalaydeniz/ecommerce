c2f37ed3094d6981ee1c8aa999433ed40abf162c
edunimbus <106919349+cemalaydeniz@users.noreply.github.com>
2024-01-14 15:38:42 +0100

    created product repository

---
+++ src/Core/ecommerce.Domain/Aggregates/ProductAggregate/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace ecommerce.Domain.Aggregates.ProductAggregate
+{
+    /// <summary>
+    /// Available CRUD operations for the aggregate root: <see cref="Product"/>
+    /// </summary>
+    public interface IProductRepository
+    {
+        Task AddAsync(Product newProduct, CancellationToken cancellationToken = default);
+        void Update(Product product);
+        Task<Product?> GetByIdAsync(Guid productId, bool getSoftDeleted, CancellationToken cancellationToken = default);
+        Task<List<Product>> GetByIdsAsync(IEnumerable<Guid> productIds, bool getSoftDeleted, CancellationToken cancellationToken = default);
+        Task<List<Product>> SearchByNameAsync(string name, int page, int pageSize, bool getSoftDeleted, CancellationToken cancellationToken = default);
+    }
+}
