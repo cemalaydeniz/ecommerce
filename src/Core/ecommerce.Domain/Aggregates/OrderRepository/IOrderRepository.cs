e7ef059c799462e47f1b65dd9f4a2ed98e143b0b
edunimbus <106919349+cemalaydeniz@users.noreply.github.com>
2024-01-14 15:48:00 +0100

    created order repository

---
+++ src/Core/ecommerce.Domain/Aggregates/OrderRepository/IOrderRepository.cs
@@ -0,0 +1,17 @@
+﻿using ecommerce.Domain.Aggregates.OrderRepository.Entities;
+
+namespace ecommerce.Domain.Aggregates.OrderRepository
+{
+    /// <summary>
+    /// Available CRUD operations for the aggregate root: <see cref="Order"/>
+    /// </summary>
+    public interface IOrderRepository
+    {
+        Task AddAsync(Order newOrder, CancellationToken cancellationToken = default);
+        Task AddTicketMessageAsync(Order order, TicketMessage ticketMessage, CancellationToken cancellationToken = default);
+        void Update(Order order);
+        Task<Order?> GetByIdAsync(Guid orderId, CancellationToken cancellationToken = default);
+        Task<List<Order>> GetOrdersofUserAsync(Guid userId, int page, int pageSize, CancellationToken cancellationToken = default);
+        Task<List<Order>> GetAllAsync(int page, int pageSize, CancellationToken cancellationToken = default);
+    }
+}
