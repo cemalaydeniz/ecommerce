8a55f9a368e34de4c81b38ef88e3dfbfc2239f47
edunimbus <106919349+cemalaydeniz@users.noreply.github.com>
2024-01-14 15:29:15 +0100

    created base of role class

---
+++ src/Core/ecommerce.Domain/Aggregates/RoleAggregate/Role.cs
@@ -0,0 +1,18 @@
+﻿using ecommerce.Domain.Aggregates.UserAggregate;
+using ecommerce.Domain.SeedWork;
+
+namespace ecommerce.Domain.Aggregates.RoleAggregate
+{
+    public class Role : BaseEntity<Guid>, IAggregateRoot
+    {
+        #region Properties
+        public string Name { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public DateTime LastUpdatedAt { get; private set; }
+
+        // Navigations
+        private List<User> _users = new List<User>();
+        public IReadOnlyCollection<User> Users => _users.AsReadOnly();
+        #endregion
+    }
+}
