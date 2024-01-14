58afdb187deb0fe53d9f25754515fa72e9623dbf
edunimbus <106919349+cemalaydeniz@users.noreply.github.com>
2024-01-14 14:50:40 +0100

    created base value object class

---
+++ src/Core/ecommerce.Domain/SeedWork/ValueObject.cs
@@ -0,0 +1,36 @@
+﻿namespace ecommerce.Domain.SeedWork
+{
+    public abstract class ValueObject : IEquatable<ValueObject>
+    {
+        public abstract IEnumerable<object> GetEqualityComponents();
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null || obj.GetType() != this.GetType())
+                return false;
+
+            var other = (ValueObject)obj;
+            return GetEqualityComponents().SequenceEqual(other.GetEqualityComponents());
+        }
+
+        public bool Equals(ValueObject? other)
+        {
+            return Equals((object?)other);
+        }
+
+        public static bool operator ==(ValueObject? left, ValueObject? right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(ValueObject? left, ValueObject? right)
+        {
+            return !Equals(left, right);
+        }
+
+        public override int GetHashCode()
+        {
+            return GetEqualityComponents().Select(x => x?.GetHashCode() ?? 0).Aggregate((x, y) => x ^ y);
+        }
+    }
+}
