ffbc0026b70afb0640bd64bcc1643fea5a034623
edunimbus <106919349+cemalaydeniz@users.noreply.github.com>
2024-01-14 15:32:23 +0100

    created role repository

---
+++ src/Core/ecommerce.Domain/Aggregates/RoleAggregate/IRoleRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace ecommerce.Domain.Aggregates.RoleAggregate
+{
+    /// <summary>
+    /// Available CRUD operations for the aggregate root: <see cref="Role"/>
+    /// </summary>
+    public interface IRoleRepository
+    {
+        Task AddAsync(Role newRole, CancellationToken cancellationToken = default);
+        void Update(Role role);
+        Task<Role?> GetByIdAsync(Guid roleId, bool includeUsers, CancellationToken cancellationToken = default);
+        Task<Role?> GetByNameAsync(string roleName, bool includeUsers, CancellationToken cancellationToken = default);
+        Task<List<Role>> GetRolesofUser(Guid userId, CancellationToken cancellationToken = default);
+        Task Delete(Guid roleId, CancellationToken cancellationToken = default);
+    }
+}
