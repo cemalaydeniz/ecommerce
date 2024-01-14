1877107b037ab08ea657c6a8ad0c9a3d1a433faa
edunimbus <106919349+cemalaydeniz@users.noreply.github.com>
2024-01-14 18:36:34 +0100

    added the service registration of the persistence layer

---
+++ src/Infrastructure/ecommerce.Persistence/ServiceRegistration.cs
@@ -0,0 +1,15 @@
+﻿using ecommerce.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ecommerce.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices(this IServiceCollection serviceProvider, IConfiguration configuration)
+        {
+            serviceProvider.AddDbContext<AppDbContext>(_ => _.UseNpgsql(configuration.GetConnectionString("Default")));
+        }
+    }
+}
