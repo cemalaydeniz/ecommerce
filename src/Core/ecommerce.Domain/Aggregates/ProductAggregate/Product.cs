e5573184fac78af3febd090016f66ad846f84613
edunimbus <106919349+cemalaydeniz@users.noreply.github.com>
2024-01-14 15:36:55 +0100

    created base of product class

---
+++ src/Core/ecommerce.Domain/Aggregates/ProductAggregate/Product.cs
@@ -0,0 +1,32 @@
+﻿using ecommerce.Domain.Common.ValueObjects;
+using ecommerce.Domain.SeedWork;
+
+namespace ecommerce.Domain.Aggregates.ProductAggregate
+{
+    public class Product : BaseEntity<Guid>, ISoftDelete
+    {
+        #region Properties
+        public string Name { get; private set; }
+        public List<Money> Prices { get; private set; }
+        public string Description { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public bool IsDeleted { get; private set; }
+        #endregion
+
+        #region Behaviors
+        /// <summary>
+        /// Marks the product as deleted
+        /// </summary>
+        /// <returns>TRUE if the product is marked as deleted successfully, FALSE if the product is already marked as deleted</returns>
+        public bool Delete()
+        {
+            if (IsDeleted)
+                return false;
+
+            IsDeleted = true;
+
+            return true;
+        }
+        #endregion
+    }
+}
