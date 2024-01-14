0c70592a39fb62f688c2fbfa307f6cde9c1b31d3
edunimbus <106919349+cemalaydeniz@users.noreply.github.com>
2024-01-14 16:25:22 +0100

    added validations for the ticket message property

--- src/Core/ecommerce.Domain/Aggregates/OrderRepository/Entities/TicketMessage.cs
+++ src/Core/ecommerce.Domain/Aggregates/OrderRepository/Entities/TicketMessage.cs
@@ -1,4 +1,5 @@
-﻿using ecommerce.Domain.SeedWork;
+﻿using ecommerce.Domain.Common.Exceptions;
+using ecommerce.Domain.SeedWork;
 
 namespace ecommerce.Domain.Aggregates.OrderRepository.Entities
 {
@@ -10,6 +11,19 @@ namespace ecommerce.Domain.Aggregates.OrderRepository.Entities
 
         // Navigations
         public Guid UserId { get; private set; }
+        #endregion
+
+        #region Validations
+        public static readonly int ContentMinLength = 10;
+        public static readonly int ContentMaxLength = 300;
+
+        private void ValidateContent(string content)
+        {
+            if (string.IsNullOrWhiteSpace(content))
+                throw new ArgumentNullException(nameof(content));
+            if (content.Length < ContentMinLength || content.Length > ContentMaxLength)
+                throw new CharLengthOutofRangeException(nameof(content), ContentMinLength, ContentMaxLength);
+        }
         #endregion
     }
 }
