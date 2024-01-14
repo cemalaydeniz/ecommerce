d57b29406462c90f76f66dc8ed63e122ef4a00a6
edunimbus <106919349+cemalaydeniz@users.noreply.github.com>
2024-01-14 19:30:51 +0100

    added jwt token service interface

---
+++ src/Core/ecommerce.Application/Authentication/JwtToken.cs
@@ -0,0 +1,9 @@
+﻿namespace ecommerce.Application.Authentication
+{
+    public class JwtToken
+    {
+        public string AccessToken { get; set; } = null!;
+        public string RefreshToken { get; set; } = null!;
+        public DateTime RefreshTokenExpirationDate { get; set; }
+    }
+}
---
+++ src/Core/ecommerce.Application/Services/IJwtTokenService.cs
@@ -0,0 +1,10 @@
+﻿using ecommerce.Application.Authentication;
+using ecommerce.Domain.Aggregates.UserAggregate;
+
+namespace ecommerce.Application.Services
+{
+    public interface IJwtTokenService
+    {
+        JwtToken GenerateToken(User user, bool newRefreshToken = false);
+    }
+}
