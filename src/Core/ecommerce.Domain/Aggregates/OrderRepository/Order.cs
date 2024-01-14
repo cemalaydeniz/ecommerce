4ac0522488c3a976a567335525ece6f0f7a22534
edunimbus <106919349+cemalaydeniz@users.noreply.github.com>
2024-01-14 15:42:16 +0100

    created base of order class

---
+++ src/Core/ecommerce.Domain/Aggregates/OrderRepository/Order.cs
@@ -0,0 +1,19 @@
+﻿using ecommerce.Domain.Aggregates.OrderRepository.Enums;
+using ecommerce.Domain.Common.ValueObjects;
+using ecommerce.Domain.SeedWork;
+
+namespace ecommerce.Domain.Aggregates.OrderRepository
+{
+    public class Order : BaseEntity<Guid>, IAggregateRoot
+    {
+        #region Properties
+        public string UserName { get; private set; }
+        public Address DeliveryAddress { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public TicketStatus TicketStatus { get; private set; }
+
+        // Navigations
+        public Guid UserId { get; private set; }
+        #endregion
+    }
+}
