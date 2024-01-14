6b6220945d09dbb3a53286834068d6f30cf0a4b9
edunimbus <106919349+cemalaydeniz@users.noreply.github.com>
2024-01-14 15:59:41 +0100

    added validations for the role properties

--- src/Core/ecommerce.Domain/Aggregates/RoleAggregate/Role.cs
+++ src/Core/ecommerce.Domain/Aggregates/RoleAggregate/Role.cs
@@ -1,4 +1,5 @@
 ﻿using ecommerce.Domain.Aggregates.UserAggregate;
+using ecommerce.Domain.Common.Exceptions;
 using ecommerce.Domain.SeedWork;
 
 namespace ecommerce.Domain.Aggregates.RoleAggregate
@@ -13,6 +14,25 @@ namespace ecommerce.Domain.Aggregates.RoleAggregate
         // Navigations
         private List<User> _users = new List<User>();
         public IReadOnlyCollection<User> Users => _users.AsReadOnly();
+        #endregion
+
+        #region Validations
+        public static readonly int NameMinLength = 3;
+        public static readonly int NameMaxLength = 20;
+
+        private void ValidateName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentNullException(nameof(name));
+            if (name.Length < NameMinLength || name.Length > NameMaxLength)
+                throw new CharLengthOutofRangeException(nameof(name), NameMinLength, NameMaxLength);
+        }
+
+        private void ValidateUser(User user)
+        {
+            if (user == null)
+                throw new ArgumentNullException(nameof(user));
+        }
         #endregion
     }
 }
