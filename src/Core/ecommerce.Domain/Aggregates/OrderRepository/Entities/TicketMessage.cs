5b41410c5db251277fb5ed5bf1bc05d21552d082
edunimbus <106919349+cemalaydeniz@users.noreply.github.com>
2024-01-14 17:51:12 +0100

    added behaviors of the ticket message class

--- src/Core/ecommerce.Domain/Aggregates/OrderRepository/Entities/TicketMessage.cs
+++ src/Core/ecommerce.Domain/Aggregates/OrderRepository/Entities/TicketMessage.cs
@@ -1,4 +1,6 @@
-﻿using ecommerce.Domain.Common.Exceptions;
+﻿#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+using ecommerce.Domain.Common.Exceptions;
 using ecommerce.Domain.SeedWork;
 
 namespace ecommerce.Domain.Aggregates.OrderRepository.Entities
@@ -11,6 +13,28 @@ namespace ecommerce.Domain.Aggregates.OrderRepository.Entities
 
         // Navigations
         public Guid UserId { get; private set; }
+        #endregion
+
+        #region Behaviors
+        private TicketMessage() { }
+
+        /// <summary>
+        /// Creates a ticket message locally
+        /// </summary>
+        /// <param name="userId">The user who sends the message</param>
+        /// <param name="content">The content of the message</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="CharLengthOutofRangeException"></exception>
+        public TicketMessage(Guid userId,
+            string content)
+        {
+            ValidateContent(content);
+
+            Id = Guid.NewGuid();
+            Content = content;
+
+            UserId = userId;
+        }
         #endregion
 
         #region Validations
