16bde0b71942f17e28446de842c5dbcbd33735ff
edunimbus <106919349+cemalaydeniz@users.noreply.github.com>
2024-01-15 12:54:55 +0100

    added unit of work fixture for tests

---
+++ test/ecommerce.TestUtility/Fixtures/UnitofWorkFixture.cs
@@ -0,0 +1,37 @@
+﻿using ecommerce.Application.UnitofWorks;
+using ecommerce.Persistence.Context;
+using ecommerce.Persistence.Interceptors;
+using ecommerce.Persistence.UnitofWorks;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+
+namespace ecommerce.TestUtility.Fixtures
+{
+    public class UnitofWorkFixture : IDisposable
+    {
+        public IUnitofWork UnitofWork { get; private set; }
+        public Mock<IMediator> MockMediator { get; private set; }
+
+        public AppDbContext _dbContext;
+
+        public UnitofWorkFixture()
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            MockMediator = new Mock<IMediator>();
+            _dbContext = new AppDbContext(options,
+                new PublishDomainEventsInterceptor(MockMediator.Object));
+
+            UnitofWork = new UnitofWork(_dbContext);
+        }
+
+        public void Dispose()
+        {
+            UnitofWork.Dispose();
+            _dbContext.Dispose();
+        }
+    }
+}
