b1d51cb506785f3055db7aaab73e2ae06414d646
edunimbus <106919349+cemalaydeniz@users.noreply.github.com>
2024-01-15 13:34:43 +0100

    added jwt token service fixture for tests

---
+++ test/ecommerce.TestUtility/Fixtures/JwtTokenServiceFixture.cs
@@ -0,0 +1,25 @@
+﻿using ecommerce.Persistence.Authentication;
+using Microsoft.Extensions.Configuration;
+
+namespace ecommerce.TestUtility.Fixtures
+{
+    public class JwtTokenServiceFixture : IDisposable
+    {
+        public JwtTokenService JwtTokenService { get; private set; }
+
+        public JwtTokenServiceFixture()
+        {
+            JwtTokenService = new JwtTokenService(new ConfigurationBuilder()
+                .AddInMemoryCollection(new Dictionary<string, string>
+                {
+                    { JwtConstants.Key_ConfigKey, StringGenerator.GenerateRandomStringStrictLength(20) },
+                    { JwtConstants.Issuer_ConfigKey, "Issuer" },
+                    { JwtConstants.Audience_ConfigKey, "Audience" },
+                    { JwtConstants.AcessTokenLifespanInMinutes_ConfigKey, "15" },
+                    { JwtConstants.RefreshTokenLifespanInDays_ConfigKey, "7" }
+                }!).Build());
+        }
+
+        public void Dispose() { }
+    }
+}
