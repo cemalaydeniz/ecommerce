54a5b95aa54eb7a05f78fdbe524773f3c664fd80
edunimbus <106919349+cemalaydeniz@users.noreply.github.com>
2024-01-15 13:35:51 +0100

    added a test for jwt token service

---
+++ test/UnitTests/ecommerce.Persistence.UnitTest/Authentication/JwtTokenServiceTest.cs
@@ -0,0 +1,27 @@
+﻿using ecommerce.DomainUnitTest.Aggregates.UserAggregate.Utilities;
+using ecommerce.TestUtility.Fixtures;
+
+namespace ecommerce.Persistence.UnitTest.Authentication
+{
+    public class JwtTokenServiceTest : IClassFixture<JwtTokenServiceFixture>
+    {
+        private readonly JwtTokenServiceFixture _jwtTokenServiceFixture;
+
+        public JwtTokenServiceTest(JwtTokenServiceFixture jwtTokenServiceFixture)
+        {
+            _jwtTokenServiceFixture = jwtTokenServiceFixture;
+        }
+
+        [Fact]
+        public void GenerateNewToken_WhenUserIsValid_ShouldReturnValidToken()
+        {
+            // Act
+            var result = _jwtTokenServiceFixture.JwtTokenService.GenerateToken(UserTestUtility.ValidUser, true);
+
+            // Assert
+            Assert.NotNull(result.AccessToken);
+            Assert.NotNull(result.RefreshToken);
+            Assert.True(result.RefreshTokenExpirationDate > DateTime.UtcNow);
+        }
+    }
+}
