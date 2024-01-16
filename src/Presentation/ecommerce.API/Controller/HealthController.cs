e8f6968dc4c477bc8f46b44f7d9e183e349f92f8
edunimbus <106919349+cemalaydeniz@users.noreply.github.com>
2024-01-16 17:02:39 +0100

    added health check

---
+++ src/Presentation/ecommerce.API/Controller/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ecommerce.API.Controller
+{
+    [ApiController]
+    [Route("health")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> HealthCheck()
+        {
+            return Ok();
+        }
+    }
+}
